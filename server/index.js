9ee5e29889bfb1e60f6e0e7f96c7b44082cbf59f
younis <lucas.mahler.mw@gmail.com>
2022-05-20 09:46:49 +0200

    able2CreatedJsonFormatUserWithPostman

--- server/index.js
+++ server/index.js
@@ -2,13 +2,19 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 
+import userRoutes from "./routes/users.js";
+
 const app = express();
 const port = 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use("/", userRoutes);
+
 // To verfiy if our server is running or not
 app.get("/", (req, res) => res.send("Hello From Express"));
+app.all("*", (req, res)=> res.send("That route dosn't exist"))
 
 app.listen(port, () => console.log(`Server is running on port: http://localhost:${port}`));
+
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,10 @@
 import express from "express";
 
-import { getUsers, createUser } from "../controllers/users";
+import { getUsers, createUser } from "../controllers/users.js";
 
 const router = express.Router();
 
 router.get("/users", getUsers);
 router.post("/users", createUser);
+
+export default router;
