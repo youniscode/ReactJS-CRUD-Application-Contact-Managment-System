e28dbd73036e2403ea5e75b224a84a2618b69cdb
younis <lucas.mahler.mw@gmail.com>
2022-05-23 05:39:39 +0200

    addedGetUserByIDFunction

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { getUsers, createUser } from "../controllers/users.js";
+import { getUsers, createUser, getUser } from "../controllers/users.js";
 
 const router = express.Router();
 
 router.get("/users", getUsers);
-router.post("/users", createUser);
+router.post("/user", createUser);
+router.get("/user/:id", getUser);
 
 export default router;
